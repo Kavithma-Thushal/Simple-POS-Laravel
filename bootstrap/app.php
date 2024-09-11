022b76cd0c3e43a360cd8be46c45101aea3585b4
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-09-11 10:15:42 +0530

    Routes Grouped Successfully...!

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
-            'customer-save-route'
+            '*',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,14 @@ use App\Http\Controllers\OrderDetailsController;
 
 Route::get('/', [DashboardController::class, 'getDashboardView'])->name('dashboard-view');
 
-Route::get('/customer-view-route', [CustomerController::class, 'getCustomerView'])->name('customer-view');
-Route::post('/customer-save-route', [CustomerController::class, 'saveCustomer'])->name('customer-save');
-Route::get('/customer-search-route', [CustomerController::class, 'searchCustomer'])->name('customer-search');
-Route::put('/customer-update-route', [CustomerController::class, 'updateCustomer'])->name('customer-update');
-Route::delete('/customer-delete-route', [CustomerController::class, 'deleteCustomer'])->name('customer-delete');
-Route::get('/customers-get-all-route', [CustomerController::class, 'getAllCustomers'])->name('customers-get-all');
+Route::controller(CustomerController::class)->group(function () {
+    Route::get('/customer-view-route', 'getCustomerView')->name('customer-view');
+    Route::post('/customer-save-route', 'saveCustomer')->name('customer-save');
+    Route::get('/customer-search-route', 'searchCustomer')->name('customer-search');
+    Route::put('/customer-update-route', 'updateCustomer')->name('customer-update');
+    Route::delete('/customer-delete-route', 'deleteCustomer')->name('customer-delete');
+    Route::get('/customers-get-all-route', 'getAllCustomers')->name('customers-get-all');
+});
 
 Route::get('/item-view-route', [ItemController::class, 'getItemView'])->name('item-view');
 Route::get('/placer-order-view-route', [PlaceOrderController::class, 'getPlaceOrderView'])->name('place-order-view');
